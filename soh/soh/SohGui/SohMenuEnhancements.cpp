b101b9a7469e621f9a838a17e00fbe779ad1797b
oinacioo <kaoriinacio@gmail.com>
2026-09-12 06:47:07 -0300

    Add DiscordMenu header to SohMenuEnhancements

--- soh/soh/SohGui/SohMenuEnhancements.cpp
+++ soh/soh/SohGui/SohMenuEnhancements.cpp
@@ -1,4 +1,5 @@
 ﻿#include "SohMenu.h"
+#include "soh/Discord/DiscordMenu.h"
 #include <soh/Enhancements/enhancementTypes.h>
 #include "soh/Enhancements/SwitchAge.h"
 #include "soh/Enhancements/AdultMasks.h"
