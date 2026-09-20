6e23b9fd2e5c04df00e7957e119c71099999d096
oinacioo <kaoriinacio@gmail.com>
2026-09-20 03:05:56 -0300

    Update SohMenuModBrowser.cpp

--- soh/soh/SohGui/SohMenuModBrowser.cpp
+++ soh/soh/SohGui/SohMenuModBrowser.cpp
@@ -1,6 +1,10 @@
 #ifdef _WIN32
-    #define WIN32_LEAN_AND_MEAN
-    #define NOMINMAX
+    #ifndef WIN32_LEAN_AND_MEAN
+        #define WIN32_LEAN_AND_MEAN
+    #endif
+    #ifndef NOMINMAX
+        #define NOMINMAX
+    #endif
     #ifndef _WIN32_WINNT
         #define _WIN32_WINNT 0x0A00
     #endif
